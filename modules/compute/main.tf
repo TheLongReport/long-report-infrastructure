1b5b4eaa9d78212d54ddca5e7a24621ed41a396d
Christopher Long <chris@quantico.xyz>
2024-06-13 00:48:51 -0400

    Fix VM disk name conflict and correct port range in NSG rules

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -22,11 +22,11 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = var.vm_name
   location              = var.location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
   vm_size               = var.vm_size
 
   storage_os_disk {
-    name              = "osdisk"
+    name              = "${var.vm_name}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -48,4 +48,6 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
+  tags = var.tags
 }
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -32,16 +32,30 @@ resource "azurerm_network_security_rule" "allow_ssh" {
   resource_group_name         = var.resource_group_name
 }
 
-resource "azurerm_network_security_rule" "allow_http_https" {
-  name                        = "AllowHTTPHTTPS"
-  priority                    = 1001
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "AllowHTTP"
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80,443"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  network_security_group_name = azurerm_network_security_group.main.name
   resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "AllowHTTPS"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.main.name
 }
