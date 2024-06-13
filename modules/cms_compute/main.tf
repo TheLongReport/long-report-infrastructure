c81571101910411ddeb8291917d5b62707b8188e
Christopher Long <chris@quantico.xyz>
2024-06-13 01:27:10 -0400

    updating the compute main.tf files to fix the init issue

--- modules/cms_compute/main.tf
+++ modules/cms_compute/main.tf
@@ -1,14 +1,3 @@
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.environment}-nic-${var.vm_name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_public_ip" "public_ip" {
   name                = var.public_ip_name
   location            = var.location
